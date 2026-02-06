61e8ba861efdc8ef4b734f611fba12de4adc49d4
Sadikul Mobasshir <114331112+MOBASSHIR07@users.noreply.github.com>
2026-02-06 23:03:45 +0600

    provider should also need category

--- src/modules/admin/admin.Route.ts
+++ src/modules/admin/admin.Route.ts
@@ -10,7 +10,7 @@ router.patch("/users/:id", authMiddleware("ADMIN"),adminController.updateStatus)
 // --- Category Management ---
 router.post("/categories", authMiddleware("ADMIN"), adminController.createCategory);
 router.get("/orders", authMiddleware("ADMIN"), adminController.getAllOrders)
-router.get("/categories", authMiddleware("ADMIN"), adminController.getAllCategories);
+router.get("/categories", authMiddleware("ADMIN", "PROVIDER"), adminController.getAllCategories);
 router.patch("/categories/:id", authMiddleware("ADMIN"), adminController.updateCategory);
 router.delete("/categories/:id", authMiddleware("ADMIN"), adminController.deleteCategory);
 
