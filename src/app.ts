225935f3008b4fc7bc423fd67391e85ac4bf4097
Sadikul Mobasshir <114331112+MOBASSHIR07@users.noreply.github.com>
2026-02-08 00:31:50 +0600

    cors changed based on frontend live url

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "http://localhost:5000", 
+  origin: "https://food-hub-frontend-a4.vercel.app",  
   credentials: true,              
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
